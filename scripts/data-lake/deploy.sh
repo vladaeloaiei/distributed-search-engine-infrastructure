c2aa10be58519f1b21b6c5e65688754f6b0287cc
Vlad Aeloaiei <vlad.aeloaiei@gmail.com>
2021-04-25 02:51:04 +0300

    Revert mongo replica set

--- scripts/data-lake/deploy.sh
+++ scripts/data-lake/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 echo "Deploying data lake stack.."
-docker stack deploy -c ./stacks/data-lake-stack.yml data-lake-2-stack
+docker stack deploy -c ./stacks/data-lake-stack.yml data-lake-stack
 echo "Waiting 10 sec.."
 sleep 10
