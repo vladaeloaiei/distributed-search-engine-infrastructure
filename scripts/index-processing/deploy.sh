ba3868a21e746f43bf4e51b1295e6712573194a9
Vlad Aeloaiei <vlad.aeloaiei@gmail.com>
2021-05-31 22:21:37 +0300

    Add Bitnami image with replication for MongoDB

--- scripts/index-processing/deploy.sh
+++ scripts/index-processing/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 echo "Deploying index processing application.."
-docker-compose -f ./apps/index-processing-app.yml -p index-processing-app up -d
+docker compose -f ./apps/index-processing-app.yml -p index-processing-app up -d
